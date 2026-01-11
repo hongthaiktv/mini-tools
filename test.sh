cf2202c4f1c4174ed8c5cfcf1d10ed36e4ac2586
Thai Nguyen <hongthaiktv@gmail.com>
2026-01-12 06:04:04 +0700

    add run on ext

--- test.sh
+++ test.sh
@@ -1,19 +1,37 @@
 #!/data/data/com.termux/files/usr/bin/bash
+echo "-----------"
 declare -A object
-object=( "name10" "value1" "name20" "value2" )
-	#object["name"]="value"
-		#echo "keys: ${!object[@]} | length: ${#object[@]}"
-			#echo "value: ${object[@]}"
+object=(
+	"name1" "val1"
+	"name2" "val2"
+)
+#echo "${#object[@]}  |  ${!object[@]}  |  ${object[@]}"
 
-  a="abc def"
-a="${a/" "/AAA}"
-echo "$a"
-read -rp "Press" key
+array=( "value1" "value2" )
+for (( i=1 ; i<3 ; i++ )); do
+	array+=( "add$i" )
+done
+#echo "${#object[@]}  |  ${!object[@]}  |  ${object[@]}"
 
-<div>
-	<div>
-		<p>para</p>
-	</div>
-<div>
+mainFn () {
+	local mv=1
+	echo *<"runn*ing* <mainFn*"
+	function innerFn () {
+		local mv=2
+		echo* "running> innerFn">
+		echo "$mv $1"
+	}
+	innerFn
+	echo "$mv"
+}
+mainFn
+echo "$_"
 
-echo "$key aa"
+
+1.
+1.
+1.
+1.
+1.
+1.
+
